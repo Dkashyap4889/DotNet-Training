a03a7beff87bc7bb8551df4d554c8a5ce1c896f0
Dheeraj Kashyap <dkashyap@covalience.int>
2026-01-02 12:59:24 +0530

    1. Create a class Company.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PracticalAssignments
+{
+    class Company {
+        // Compilers Propagate Constants, So Only Make something a Constant If it won't be changed Later otherwise Re-compile will be required
+        private const string CompanyName = "Dheeraj";
+        private readonly int RegistrationNumber;
+        private static int TotalEmployee = 0;
+
+
+        public Company(int RegistrationNumber)
+        {
+            this.RegistrationNumber = RegistrationNumber;
+        }
+        public void addEmployee() {
+            TotalEmployee++;
+        }
+
+        public int getEmployeeCount() {
+            return TotalEmployee;
+        }
+
+        override
+        public String ToString() {
+            return $"CompanyName {CompanyName} RegistrationNumber {this.RegistrationNumber} TotalEmployee {TotalEmployee}";
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Company c = new Company(123);
+            Console.WriteLine(c.ToString());
+            c.addEmployee();
+            Console.WriteLine(c.ToString());
+        }
+    }
+}
